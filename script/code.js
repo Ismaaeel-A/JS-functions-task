c64ab672d2b4f31d95e7dbc27a02ea4493d1729d
Ismaaeel-A <ismaaeelahmed42@gmail.com>
2024-05-23 09:43:41 +0200

    complete

---
+++ script/code.js
@@ -0,0 +1,17 @@
+//Question 1
+function greet(greeting){
+    console.log(greeting)
+}
+greet('Hello')
+
+//Question 2
+function multiply(num1, num2){
+console.log(num1*num2)
+}
+multiply(4, 8)
+
+//Question 3
+function tableOfFive(numb){
+console.log(numb*5)
+}
+tableOfFive(6)
